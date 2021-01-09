bdd6222e27e7dfa5f2c6481d0abc391091669938
goncalomotaribeiro <goncalomota.cr@gmail.com>
2021-01-09 15:22:35 +0000

    ss

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,30 @@
 <template>
   <div id="nav">
     <b-navbar toggleable="lg" type="dark">
-      <b-navbar-brand href="#"><b-img src="../assets/logo.png" fluid alt="Fluid image"></b-img></b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#" class="offset-lg-1"><b-img src="../assets/logo.png" fluid alt="Fluid image"></b-img></b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse" ></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
+      <b-collapse id="nav-collapse" class="offset-lg-7 justify-content-center" is-nav>
         <b-navbar-nav  v-if="!this.$store.getters.isLoggedUser">
           <b-nav-item><router-link to="/">Entrar</router-link></b-nav-item>
           <b-nav-item><router-link to="/register">Registar</router-link></b-nav-item>
         </b-navbar-nav>
 
-        <b-navbar-nav  v-else>
-          <b-nav-item><router-link @click.native='logout' to="/">Sair</router-link></b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form v-if="this.$store.getters.isLoggedUser">
+        <b-navbar-nav class="ml-auto" v-if="this.$store.getters.isLoggedUser">
+          <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
           </b-nav-form>
 
-          <b-nav-item-dropdown right v-if="this.$store.getters.isLoggedUser">
-            <!-- Using 'button-content' slot -->
+          <b-nav-item-dropdown v-if="this.$store.getters.isLoggedUser">
             <template #button-content>
               <em>User</em>
             </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#">Editar Perfil</b-dropdown-item>
+            <b-dropdown-item @click.native='logout' to="/">Sair</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
+
       </b-collapse>
     </b-navbar>
   </div>
@@ -49,8 +44,6 @@ export default {
 <style>
 
   #nav {
-    display: flex;
-    justify-content: space-around;
     padding: 0;
     background-color: #ffffff;
     color: #000000;
@@ -58,13 +51,27 @@ export default {
   }
 
   #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-    display: inline;
+    text-decoration: none;
+    font-weight: 500;
+    font-family: 'Segoe UI';
+    color: #000000;
     transition: all 0.3s ease 0s;
   }
 
+  #nav .nav-link, #button-content {
+    margin-left: 20px;
+    background-color: white;
+    border-radius: 8px;
+    border: 1px solid black;
+    padding: 2px;
+    padding-inline: 25px;
+  }
+
+  #nav .nav-link:hover {
+    background-color: rgb(255, 230, 255);
+  }
+
   #nav a.router-link-exact-active {
-    color: #42b983;
+    color: #000000;
   }
 </style>
