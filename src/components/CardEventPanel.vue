63e1c8465599e76602c7cecb13d323a6f0aeb170
goncalomotaribeiro <goncalomota.cr@gmail.com>
2021-01-23 21:34:07 +0000

    dd

--- src/components/CardEventPanel.vue
+++ src/components/CardEventPanel.vue
@@ -1,45 +1,47 @@
 <template>
-    <b-row class="cardChallenge mt-5">
-        <b-col cols="lg-2" class="text-xl-left">
-            <b-img :src="getEvent.img"  alt="Fluid image" class="imgUserChallenge"></b-img>
-        </b-col>
-        <b-col cols="0" class="d-flex flex-column justify-content-between text-xl-left">
-            <b-row>
-                <b-col class="titleChallenge">{{getEvent.title}}</b-col>
-            </b-row>
-            <b-row>
-                <b-col class="state mb-2">Aberto</b-col>
-            </b-row>
-            <b-row>
-                <b-col cols="0" :style="{'background-color': getScientificArea.color}" class="category ml-3">{{getScientificArea.name}}</b-col>
-            </b-row>
-        </b-col>
-        <div class="linha align-self-center d-none d-xl-block" style="margin-left:90px"></div>
-        <b-col cols="0" class="d-flex flex-column justify-content-around ml-5 mt-2 mb-2">
-            <b-row>
-                <b-col class="time1">{{getEvent.date}}</b-col>
-            </b-row>
-            <b-row>
-                <b-col class="time2">Data</b-col>
-            </b-row>
-        </b-col>
-        <div class="linha align-self-center ml-5 d-none d-xl-block"></div>
-        <b-col class="align-self-center text-xl-left ml-5">
-            <b-row>
-                <b-col class="description">{{ getDescription(getEvent.description)}}</b-col>
-            </b-row>
-        </b-col>
-        <b-col sm class="p-0 align-self-center">
-            <b-row>
-                <b-col>
-                    <b-button id="btnSubmit" class="btnSubmitStyle" to="/panel/my-events">
-                        Remover
-                    </b-button>
-                </b-col>
-            </b-row>
-            <router-link :to="{name: 'Event', params:{eventId: getEvent.id}}" class="stretched-link" ></router-link>
-        </b-col>
-    </b-row>
+    <b-link :to="{name: 'Event', params:{eventId: getEvent.id}}">
+        <b-row class="cardChallenge mt-5">
+            <b-col cols="lg-2" class="text-xl-left">
+                <b-img :src="getEvent.img"  alt="Fluid image" class="imgUserChallenge"></b-img>
+            </b-col>
+            <b-col cols="0" class="d-flex flex-column justify-content-between text-xl-left">
+                <b-row>
+                    <b-col class="titleChallenge">{{getEvent.title}}</b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="state mb-2">Aberto</b-col>
+                </b-row>
+                <b-row>
+                    <b-col cols="0" :style="{'background-color': getScientificArea.color}" class="category ml-3">{{getScientificArea.name}}</b-col>
+                </b-row>
+            </b-col>
+            <div class="linha align-self-center d-none d-xl-block" style="margin-left:90px"></div>
+            <b-col cols="0" class="d-flex flex-column justify-content-around ml-5 mt-2 mb-2">
+                <b-row>
+                    <b-col class="time1">{{getEvent.date}}</b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="time2">Data</b-col>
+                </b-row>
+            </b-col>
+            <div class="linha align-self-center ml-5 d-none d-xl-block"></div>
+            <b-col class="align-self-center text-xl-left ml-5">
+                <b-row>
+                    <b-col class="description">{{ getDescription(getEvent.description)}}</b-col>
+                </b-row>
+            </b-col>
+            <b-col sm class="p-0 align-self-center">
+                <b-row>
+                    <b-col>
+                        <b-button @click="deleteMyEvent" id="btnSubmit" class="btnSubmitStyle" to="/panel/my-events">
+                            Remover
+                        </b-button>
+                    </b-col>
+                </b-row>
+                <router-link  class="stretched-link" ></router-link>
+            </b-col>
+        </b-row>
+    </b-link>
 </template>
 
 <script>
@@ -59,7 +61,10 @@ export default {
     methods: {
         getDescription(desc) {
             return desc.substring(0, 50) + '...';
-        }
+        },
+        deleteMyEvent() {
+            this.$store.dispatch("deleteMyEvent", this.myEvent.event);
+        },
     }
 }
 </script>
@@ -84,7 +89,7 @@ export default {
 #myEvents .btnSubmitStyle{
     border: 2px solid black;
     font-weight: 600;
-    padding: 3px 10px 3px 10px;
+    padding: 3px 15px 3px 15px;
     color: black;
     font-family: 'Segoe UI';
     border-radius: 14px;
@@ -136,5 +141,9 @@ export default {
     color: black;
     font-family: 'Segoe UI';
     font-size: 17px;
+}
+
+#myEvents a {
+    text-decoration: none;
 }
 </style>
---
+++ src/views/subviews/Event.vue
@@ -0,0 +1,171 @@
+<template>
+    <div id="event">
+        <b-container fluid class="cover" :style="{backgroundImage:`url(${getEvent.img})`}">
+        </b-container>
+
+        <b-container>
+            <b-row>
+                <b-col cols="4" class="mt-5">
+                    <b-img class="profile-picture mt-3 mb-3" src="../../assets/picture-teacher.png" fluid alt="Fluid image"></b-img>
+                    <p class="name mb-2">Rodrigo Moreira</p>
+                    <p class="type mb-3">Docente</p>
+                    <b-button class="follow">Seguir</b-button>
+                </b-col>
+                <b-col cols="0">
+                    <div class="linha align-self-center d-none d-xl-block"></div>
+                </b-col>
+                <b-col cols="7" class="text-left ml-5 info">
+                    <span class="challengeName">{{getEvent.title}}</span>
+                    <span class="name d-flex justify-content-end">{{getEvent.date}}</span>
+                    <p class="challengeDescript mt-3">{{getEvent.description}}</p>
+                    <b-button @click="onSubmit" id="btnSubmit" class="btnSubmitStyle mt-3" v-if="saved">
+                        Guardar
+                        <b-img src="../../assets/arrow-right-xs.png" fluid alt="Fluid image" class="ml-5 mb-1"></b-img>
+                    </b-button>
+                    <b-button @click="deleteMyEvent" id="btnSubmited" class="btnSubmitedStyle mt-3" v-else>
+                        Guardado
+                    </b-button>
+
+                    <b-button :href="getEvent.link" target="_blank" id="btnSubscribe" class="btnSubmitStyle mt-3 ml-4">
+                        Inscrever
+                        <b-img src="../../assets/arrow-right-xs.png" fluid alt="Fluid image" class="ml-5 mb-1"></b-img>
+                    </b-button>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Event',
+    data() {
+        return {
+        }
+    },
+    methods: {
+        onSubmit() {
+            const myEvent = {
+            id: this.$store.getters.getNextMyEventId,
+            event: this.$route.params.eventId,
+            };
+            this.$store.dispatch("insertMyEvent", myEvent);
+        },
+        deleteMyEvent() {
+            this.$store.dispatch("deleteMyEvent", this.$route.params.eventId);
+        },
+    },
+    computed: {
+        getEvent() {
+        return this.$store.getters.getEventById(this.$route.params.eventId);
+        },
+        saved(){
+
+            for (const myEvent of this.$store.getters.getMyEvents) {
+                if (this.$route.params.eventId == myEvent.event) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
+</script>
+<style>
+    #event .cover{
+        height: 230px;
+        margin-top: 67px;
+        border-radius: 15px;
+        box-shadow: #ebceff 6px 6px;
+        border: 1px solid #ebceff;
+        background-position: center;
+        background-size: cover;
+    }
+
+    #event .name{
+        font-family: 'Segoe UI';
+        font-weight: 600;
+        color: black;
+        font-size: 15pt;
+        margin: 0;
+    }
+
+    #event .type{
+        font-weight: 400;
+        font-family: 'Segoe UI';
+        color: black;
+        font-size: 11pt;
+        margin: 0;
+    }
+
+    #event .follow{
+        background-color: #A7F6FF;
+        border: 2px solid black;
+        border-radius: 6px;
+        padding: 3px 25px 3px 25px;
+        font-weight: 500;
+        font-family: 'Segoe UI';
+        color: black;
+        font-size: 12pt;
+        margin: 0;
+    }
+
+    #event .linha{
+        height: 230px;
+        border-right: 1px solid #dddddd;
+        margin-top: 70px;
+    }
+
+    #event .challengeName{
+        font-weight: 700;
+        font-family: 'Segoe UI';
+        color: black;
+        font-size: 20pt;
+    }
+
+    #event .info{
+        padding-top: 70px;
+    }
+
+     #event .challengeDescript{
+        font-weight: 500;
+        font-family: 'Segoe UI';
+        color: black;
+        font-size: 12pt;
+    }
+
+    #event .btnSubmitStyle{
+        background-color: white;
+        border: 2px solid black;
+        font-weight: 600;
+        padding: 3px 10px 3px 10px;
+        color: black;
+        font-family: 'Segoe UI';
+        border-radius: 14px;
+        font-size: 16px;
+    }
+
+    #event .btnSubmitedStyle{
+        background-color:  #DEE5FF;
+        border: 2px solid black;
+        font-weight: 600;
+        padding: 3px 10px 3px 10px;
+        color: black;
+        font-family: 'Segoe UI';
+        border-radius: 14px;
+        font-size: 16px;
+    }
+
+    @media only screen and (min-width: 1000px) {
+    #event .cover{
+        width: 1000px;
+    }
+
+        
+    @media only screen and (min-width: 1400px) {
+         #event .profile-picture{
+            width: 30%;
+        }
+    }
+
+}
+</style>
