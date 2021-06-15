637b30f1e7d1d45717341873d6aab8f381b25c0c
goncalomotaribeiro <goncalomota.cr@gmail.com>
2021-06-15 15:46:10 +0100

    cc

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
-// const API_URL = "https://webp2-api.herokuapp.com";
-const API_URL = "http://localhost:8081";
+const API_URL = "https://webp2-api.herokuapp.com";
+// const API_URL = "http://localhost:8081";
 export default API_URL;
